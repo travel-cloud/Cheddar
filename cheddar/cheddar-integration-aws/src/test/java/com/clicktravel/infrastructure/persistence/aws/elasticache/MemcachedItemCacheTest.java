30a1ab13d2e87aa23d01d4176491cb27ae37580a
Steffan Westcott <steffan.westcott@clicktravel.com>
2014-11-05 16:41:13 +0000

    FC-69 Update to Spring 4; Fix for @Retryable exception handlers
    This is a fix to enable exception handlers referenced by @Retryable methods to use annotated features, such as @Transactional and @ContinueResult. This required an upgrade from Spring 3 to 4, to enable proxying of classes (rather than just interfaces).
    
    Signed-off-by: Steffan Westcott <steffan.westcott@clicktravel.com>

--- cheddar/cheddar-integration-aws/src/test/java/com/clicktravel/infrastructure/persistence/aws/elasticache/MemcachedItemCacheTest.java
+++ cheddar/cheddar-integration-aws/src/test/java/com/clicktravel/infrastructure/persistence/aws/elasticache/MemcachedItemCacheTest.java
@@ -17,6 +17,7 @@
 package com.clicktravel.infrastructure.persistence.aws.elasticache;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -44,48 +45,71 @@ public class MemcachedItemCacheTest {
         memcachedCacheStore = new MemcachedItemCache(client);
     }
 
+    @SuppressWarnings("unchecked")
     @Test
     public void shoudlGetItem() throws InterruptedException, TimeoutException, ExecutionException {
-
+        // Given
         final String key = Randoms.randomString();
         final String item = Randoms.randomString();
         final int timeout = Randoms.randomInt(5) + 1;
-
         final GetFuture<Object> f = mock(GetFuture.class);
         when(client.asyncGet(key)).thenReturn(f);
         when(f.get(timeout, TimeUnit.SECONDS)).thenReturn(item);
+
+        // When
         final Object obj = memcachedCacheStore.getItem(key, timeout);
 
+        // Then
         assertEquals(item, obj);
-
     }
 
+    @SuppressWarnings("unchecked")
     @Test
-    public void shoudlNotGetItem_throwsTimout() throws InterruptedException, ExecutionException, TimeoutException {
-
+    public void shoudlNotGetItem_throwsTimeout() throws InterruptedException, ExecutionException, TimeoutException {
+        // Given
         final String key = Randoms.randomString();
         final int timeout = Randoms.randomInt(5) + 1;
         final GetFuture<Object> f = mock(GetFuture.class);
         when(client.asyncGet(key)).thenReturn(f);
         when(f.get(timeout, TimeUnit.SECONDS)).thenThrow(TimeoutException.class);
+
+        // When
         final Object item = memcachedCacheStore.getItem(key, timeout);
+
+        // Then
         assertNull(item);
     }
 
     @Test
     public void shouldPutItem() {
+        // Given
         final String key = Randoms.randomString();
         final String item = Randoms.randomString();
         final long expire = (long) Randoms.randomInt(5) + 1;
+
+        // When
         memcachedCacheStore.putItem(key, item, expire);
+
+        // Then
         verify(client).set(key, (int) expire, item);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void shouldNotPutItem_failExpireToLarge() {
+        // Given
         final String key = Randoms.randomString();
         final String item = Randoms.randomString();
         final long expire = (long) Integer.MAX_VALUE + 1;
-        memcachedCacheStore.putItem(key, item, expire);
+
+        // When
+        IllegalArgumentException actualException = null;
+        try {
+            memcachedCacheStore.putItem(key, item, expire);
+        } catch (final IllegalArgumentException e) {
+            actualException = e;
+        }
+
+        // Then
+        assertNotNull(actualException);
     }
 }
