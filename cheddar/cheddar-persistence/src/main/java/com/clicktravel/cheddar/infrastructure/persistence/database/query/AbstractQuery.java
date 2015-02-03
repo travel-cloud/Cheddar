d321c646247e54a9819d3a838a9a772c29b6b0c0
Basharat Janjua <basharat.janjua@clicktravel.com>
2015-02-03 07:57:52 +0000

    Implement hash-code and equals for Document Search Engine classes.

--- cheddar/cheddar-persistence/src/main/java/com/clicktravel/cheddar/infrastructure/persistence/database/query/AbstractQuery.java
+++ cheddar/cheddar-persistence/src/main/java/com/clicktravel/cheddar/infrastructure/persistence/database/query/AbstractQuery.java
@@ -29,4 +29,34 @@ public class AbstractQuery implements Query {
         return condition;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (condition == null ? 0 : condition.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AbstractQuery other = (AbstractQuery) obj;
+        if (condition == null) {
+            if (other.condition != null) {
+                return false;
+            }
+        } else if (!condition.equals(other.condition)) {
+            return false;
+        }
+        return true;
+    }
+
 }
