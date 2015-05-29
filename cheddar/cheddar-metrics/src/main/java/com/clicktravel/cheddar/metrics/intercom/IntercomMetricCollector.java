0b34c9f4554c4ddf722e4e1a41680031aea4a223
Chris Howell <chris.howell@clicktravel.com>
2015-05-29 13:57:58 +0100

    [hack] implementation of metric interface for intercom
    Signed-off-by: Chris Howell <chris.howell@clicktravel.com>

---
+++ cheddar/cheddar-metrics/src/main/java/com/clicktravel/cheddar/metrics/intercom/IntercomMetricCollector.java
@@ -0,0 +1,40 @@
+package com.clicktravel.cheddar.metrics.intercom;
+
+import com.clicktravel.cheddar.metrics.Metric;
+import com.clicktravel.cheddar.metrics.MetricCollector;
+import com.clicktravel.cheddar.metrics.MetricOrganisation;
+import com.clicktravel.cheddar.metrics.MetricUser;
+
+public class IntercomMetricCollector implements MetricCollector {
+
+    @Override
+    public void createOrganisation(final MetricOrganisation metricOrganisation) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void updateOrganisation(final MetricOrganisation metricOrganisation) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void createUser(final MetricUser user) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void updateUser(final MetricUser user) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void sendMetric(final Metric metric) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
