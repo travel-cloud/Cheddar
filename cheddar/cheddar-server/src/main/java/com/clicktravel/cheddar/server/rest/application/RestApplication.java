05ce8841d0644aecb25e6f6bfb4ae8f7f1ad2396
Steffan Westcott <steffan.westcott@clicktravel.com>
2016-08-25 23:31:42 +0100

    Workaround for shutdown issues with some applications
    Signed-off-by: Steffan Westcott <steffan.westcott@clicktravel.com>

--- cheddar/cheddar-server/src/main/java/com/clicktravel/cheddar/server/rest/application/RestApplication.java
+++ cheddar/cheddar-server/src/main/java/com/clicktravel/cheddar/server/rest/application/RestApplication.java
@@ -55,6 +55,7 @@ public class RestApplication {
             logger.info("Java process starting");
             logger.debug(String.format("java.version:[%s] java.vendor:[%s]", System.getProperty("java.version"),
                     System.getProperty("java.vendor")));
+            @SuppressWarnings("resource")
             final ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext(
                     "applicationContext.xml");
             logger.debug("Finished getting ApplicationContext");
@@ -63,8 +64,6 @@ public class RestApplication {
             Runtime.getRuntime().addShutdownHook(new Thread(() -> {
                 logger.info("Shutdown hook invoked - Commencing graceful termination of Java process");
                 applicationLifecycleController.shutdownApplication();
-                logger.debug("Closing application context");
-                applicationContext.close();
                 logger.info("Java process terminating");
             }));
             applicationLifecycleController.startApplication(servicePort, statusPort, bindAddress);
