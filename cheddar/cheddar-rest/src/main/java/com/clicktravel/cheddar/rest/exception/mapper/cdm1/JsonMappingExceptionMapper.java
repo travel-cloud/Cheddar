73649e55f84761e3075239b4d5a2b0456f199254
click-alexvladut <alexandru.vladut@clicktravel.com>
2016-11-14 15:29:24 +0200

    [FC-129] Add specific mappers for subclasses of JsonProcessingException to override the standard mappers provided by Jersey

--- cheddar/cheddar-rest/src/main/java/com/clicktravel/cheddar/rest/exception/mapper/cdm1/JsonMappingExceptionMapper.java
+++ cheddar/cheddar-rest/src/main/java/com/clicktravel/cheddar/rest/exception/mapper/cdm1/JsonMappingExceptionMapper.java
@@ -18,14 +18,21 @@ package com.clicktravel.cheddar.rest.exception.mapper.cdm1;
 
 import static com.clicktravel.cheddar.rest.exception.mapper.cdm1.JsonProcessingExceptionMapperUtils.buildErrorResponse;
 
+import javax.annotation.Priority;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.clicktravel.cheddar.rest.media.MediaTypes;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
+@Provider
+@Produces(MediaTypes.CDM_V1_JSON)
+@Priority(Integer.MAX_VALUE)
 public class JsonMappingExceptionMapper implements ExceptionMapper<JsonMappingException> {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
