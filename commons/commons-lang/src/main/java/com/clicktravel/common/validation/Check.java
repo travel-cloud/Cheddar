cfbffd5e69778c1913b10c263224b254c42d0229
Chris Hern <chris.hern@clicktravel.com>
2018-10-24 13:03:45 +0100

    Improve email validation regex to highlight more invalid addresses

--- commons/commons-lang/src/main/java/com/clicktravel/common/validation/Check.java
+++ commons/commons-lang/src/main/java/com/clicktravel/common/validation/Check.java
@@ -36,7 +36,8 @@ public class Check {
     private final static String DOES_NOT_MATCH_PATTERN = "Value does not match pattern";
     private final static Pattern PHONE_NUMBER_PATTERN = Pattern.compile("\\+[\\d -]+");
     private final static Pattern EMAIL_ADDRESS_PATTERN = Pattern.compile(
-            "^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$");
+            "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",
+            Pattern.CASE_INSENSITIVE);
 
     /**
      * Check that a string value is not empty (after trimming) or <code>null</code>
